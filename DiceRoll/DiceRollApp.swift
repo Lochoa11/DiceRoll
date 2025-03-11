d763c01aedd5345511fe9191a6b6dab51655932c
Lochoa11 <linochoa11690@gmail.com>
2025-03-11 10:47:34 -0400

    Initial Commit

---
+++ DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by Lin Ochoa on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceRollTests/DiceRollTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollTests.swift
+//  DiceRollTests
+//
+//  Created by Lin Ochoa on 3/11/25.
+//
+
+import Testing
+@testable import DiceRoll
+
+struct DiceRollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
